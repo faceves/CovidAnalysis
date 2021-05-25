1e590d4d5adc4653b09a693e41101695c5f6471c
Francisco Aceves <f_ace76@hotmail.com>
2021-05-24 19:44:03 -0700

    created variables to load data in its respective Dataframes, Datasets need to be decided id we want to use them. Also added a LoadPath object with hdfs_path variable that is in the .gitignore file to be able to use a consistent variable across the team.

--- src/main/scala/com/revature/CovidAnalysis/Runner.scala
+++ src/main/scala/com/revature/CovidAnalysis/Runner.scala
@@ -1,6 +1,8 @@
 package com.revature.CovidAnalysis
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import loadpath.LoadPath
 
 object Runner {
 
@@ -10,6 +12,62 @@ object Runner {
       .appName("Covid Analysis")
       .master("local[*]")
       .getOrCreate()
+    import spark.implicits._
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val covid_analysis_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "covid_19_data.csv")
+      .toDF()
+
+    val covid_confirmed_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "time_series_covid_19_confirmed.csv")
+      .toDF()
+
+    val covid_confirmed_US_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "time_series_covid_19_confirmed_US.csv")
+      .toDF()
+
+    val covid_deaths_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "time_series_covid_19_deaths.csv")
+      .toDF()
+
+    val covid_deaths_US_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "time_series_covid_19_deaths_US.csv")
+      .toDF()
+
+    val covid_recovered_DB = spark.read
+      .option("header", true)
+      .option("delimiter",",")
+      .format("csv")
+      .load(LoadPath.hdfs_path + "time_series_covid_19_recovered.csv")
+      .toDF()
+
+    covid_analysis_DB.show()
+    covid_confirmed_US_DB.show()
+    covid_confirmed_DB.show()
+    covid_deaths_DB.show()
+    covid_deaths_US_DB.show()
+    covid_recovered_DB.show()
+
+
+    spark.close()
   }
+
+
 
 }
