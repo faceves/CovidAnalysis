d231092bbbee77552827f1f413f582aa65002595
Metal <gfkotzabassis@gmail.com>
2021-05-26 17:34:56 -0500

    Added a method that looks at the percent confirmed-case growth and retrieves all instances of provinces/states whose growth rate is above a user-provided threshold. It is called spikeAtTarget, and it takes 5 parameters: a dataframe representing the data observed; a String representing the target column to observe; a String value equal tot the field value of the target column to observe; a date that you want to 'query on' (i.e. how many confirmed cases after USA Valentine's Day (2/14/xx)?); a range of days to observe the 'spike'; a percentage as a decimal representing a threshold for distinguishing spikes.
    By Default, the incubation period of COVID-19 is approximately 10-14 days, so we say that, the initial date of a spike is 14 days after your target day provided, and the date range provided is the number of days after your initial date plus 14. As listed above, suppose you want to know who got COVID from Valentine's Day. You would give 2/14/20 (or 21), and the program would look at days starting with February 28. If you say the range is 7 days, then from February 28th to March 6th is the time scale for the percent growth in confirmed cases. Each province/state in the region/country has a corresponding growth rate, if such a separation is given.

--- src/main/scala/com/revature/CovidAnalysis/Runner.scala
+++ src/main/scala/com/revature/CovidAnalysis/Runner.scala
@@ -1,14 +1,16 @@
 package com.revature.CovidAnalysis
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Column, SparkSession}
 import loadpath.LoadPath
-import org.apache.spark.sql.sources.And
+import org.apache.spark.sql.functions.col
+//import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.sources._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{array, col, desc, explode, lit, struct}
-import org.apache.spark.sql.types.IntegerType
 
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
@@ -67,10 +69,14 @@ object Runner {
       .load(LoadPath.hdfs_path + "time_series_covid_19_recovered.csv")
       .toDF()
 
-    val california = covid_accum_DB.where($"Country/Region" === "US" && $"Province/State" === "California")
-      .select($"Deaths".cast(IntegerType), $"ObservationDate").orderBy(desc("Deaths"))
-    california.show()
+    //val california = covid_accum_DB.where($"Country/Region" === "US" && $"Province/State" === "California")
+    //  .select($"Deaths".cast(IntegerType), $"ObservationDate").orderBy(desc("Deaths"))
+    //california.show()
 
+    //val russianConfirmation = covid_analysis_DB.filter($"Country/Region" === "Russia").select($"Country/Region", $"Deaths".cast(IntegerType)).groupBy("Country/Region").sum("Deaths")
+    val trueTotalDeaths = covid_accum_DB.select($"Country/Region", $"Deaths".cast(LongType)).groupBy("Country/Region").sum("Deaths") //tally up Casualties by country
+    val trueTotalConfirmed = covid_accum_DB.select($"Country/Region", $"Confirmed".cast(LongType)).groupBy("Country/Region").sum("Confirmed")//tally up Confirmations by country
+    val trueTotalRecovered = covid_accum_DB.select($"Country/Region", $"Recovered".cast(LongType)).groupBy("Country/Region").sum("Recovered") //tally up Recoveries by country
 
     /**
     covid_accum_DB.show()
@@ -81,10 +87,22 @@ object Runner {
     covid_recovered_DB.show()
     **/
 
-    var stateName = "New Hampshire"
-    var date = "11/1/20"
-    var stateDF = covid_confirmed_US_DB.filter('Province_State===stateName)
-    changeOverTime(stateDF, date, 14).show
+    spikeAtTarget(covid_accum_DB, "Country/Region", "Brazil", "01/01/2021", 7, 5.0)
+
+    //val joina = trueTotalDeaths.withColumnRenamed("sum(Deaths)","Deaths").join(trueTotalRecovered,Seq("Country/Region"), "Left").withColumnRenamed("sum(Recovered)", "Recovered") //Unite Deaths and Recoveries
+    //val joinb = joina.join(trueTotalConfirmed, Seq("Country/Region"), "Left").withColumnRenamed("sum(Confirmed)","Confirmed")   //Unite Deaths, Recoveries and Confirmations
+
+    //val ratio = joinb.withColumn("Decimal Deaths", joinb.col("Deaths").cast(LongType)/joinb.col("Confirmed").cast(LongType)) .withColumn("Decimal Recoveries", joinb.col("Recovered").cast(LongType)/joinb.col("Confirmed").cast(LongType))
+
+    //joinb.show()
+    //val FiveMostAffected = ratio.orderBy(desc("Confirmed")).show(5)
+    //val FiveLeastAffected = ratio.orderBy(asc("Confirmed")).show(5)
+
+    //val FiveMostDeaths = ratio.orderBy(desc("Decimal Deaths")).show(5)
+    //val FiveLeastDeaths = ratio.orderBy(asc("Decimal Deaths")).show(5) ).show
+
+    //val FiveMostRecoveries = ratio.orderBy(desc("Decimal Recoveries")).show(5)
+    //val FiveLeastRecoveries = ratio.orderBy(asc("Decimal Recoveries")).show(5)
 
     spark.close()
   }
@@ -101,6 +119,83 @@ object Runner {
     val df_temp = df.select(df("Combined_Key").as("_n_"), df(dt).cast(IntegerType)).orderBy(desc(dt))
     df_mod = df_mod.join(df_temp, df_temp("_n_")===df_mod("Combined_Key"), "inner").drop("_n_")
     df_mod.withColumn("delta",df_mod(dt)-df_mod(startDate))
+  }
+
+  def spikeAtTarget(dfFiller:DataFrame, targetFilter:String, target:String, initialDate:String, dateRange:Int, spikeFactor:Double):Unit={ //what to Return?
+    // dfFiller is the dataframe we want to look into
+    // targetFilter is the column we want to target
+    // target is the value of the column set we want to look through
+    // startDate is an arbitrary date we want to look at, for example June 12th is Brazilian Valentine's Day
+    // dateRange is the number of days after the startDate. Incubation is typically at most 14 days
+    //      then, set dateRange to 10-14 to see how many people are actually getting infected by it
+    //      alternatively, set the date to the 10th and look at 18 days for date Range to get a wider growth scope
+    // spikeFactor is the facter by which some confirmed-case growth is increased
+    //      (i.e. spikeFactor = 3.0, in 14 days we go from 10 -> 25 means the growth factor is 2.5 < 3.0 and so it does not 'count' as a spike)
+
+    var spikeShell = dfFiller.select("*").where(col(s"${targetFilter}") === s"${target}") //create DF from input DF as data filtered by target
+    //spikeShell.show()
+    val startDate = twoWeekCatcher(dfFiller, initialDate, 14)
+    val endDate = twoWeekCatcher(dfFiller, initialDate, dateRange + 14) // get date after a number of days equal to dateRange
+
+    println(s"$startDate")
+    println(s"$endDate")
+
+    var startColName = ""
+    var endColName = ""
+    var spikeStart = spikeShell
+    var spikeEnd = spikeShell
+
+    if (dfFiller.schema.fieldNames.contains("ObservationDate")){
+      println(" 'this' ")
+      startColName = "ObservationDate"
+      endColName = "ObservationDate"
+      spikeStart = spikeShell.select(col("Confirmed").as("Start Confirmed"),col("Country/Region").as("CR"),col("Province/State").as("PS")).where(col(s"${startColName}") === s"${startDate}" && col(s"$targetFilter") === s"$target")// get confirmed cases on start date
+      spikeEnd = spikeShell.select(col("Confirmed").as("End Confirmed"),col("Country/Region").as("CR"),col("Province/State").as("PS")).where(col(s"${endColName}") === s"${endDate}" && col(s"$targetFilter") === s"$target")// get confirmed cases on end date
+    }
+
+    else{
+      println(" 'that' ")
+      startColName = startDate
+      endColName = endDate
+      spikeStart = spikeShell.select(col(s"${startColName}").as("Start Confirmed"),col("Country/Region").as("CR"),col("Province/State").as("PS")).where(col(s"$targetFilter") === s"$target")// get confirmed cases on start date
+      spikeEnd = spikeShell.select(col(s"${endColName}").as("End Confirmed"),col("Country/Region").as("CR"),col("Province/State").as("PS")).where(col(s"$targetFilter") === s"$target")// get confirmed cases on end date
+    }
+
+    println(" 'Existential Terror Noises' ")
+    spikeStart.show(false)
+    println("I can't take it anymore!")
+    spikeEnd.show(false)
+
+    val spikeTime = spikeStart.join(spikeEnd, Seq("CR","PS"),"left").withColumn("SpikeFactor", ((spikeEnd.col("End Confirmed").cast(LongType)/spikeStart.col("Start Confirmed").cast(LongType)).minus(1)).multiply(100))
+    println("WI just wanna DIE~!")
+    spikeTime.select("*").show(false)
+    val check = spikeTime.select("*").where(col("SpikeFactor") >= spikeFactor)
+    println("we ALL wanna die!")
+    check.show(false)
+  }
+
+
+
+  def twoWeekCatcher(dfx:DataFrame, start:String, spread:Int):String={
+    var startDate=""
+    var pattern = ""
+    if(dfx.schema.fieldNames.contains("ObservationDate")){
+      val intermid = dfx.select("ObservationDate").where(col("ObservationDate") === s"${start}").collect
+      startDate = intermid(0)(0).toString
+      println(s"$startDate")
+      pattern = "mm/dd/yyyy"
+    }
+    else {
+      startDate=start
+      pattern = "M/d/yy"
+    }
+      val format = new SimpleDateFormat(pattern)
+      val date = format.parse(startDate)
+      val c = Calendar.getInstance()
+      c.setTime(date)
+      c.add(Calendar.DATE, spread)
+      val dt = format.format(c.getTime())
+      dt
   }
 
 }
