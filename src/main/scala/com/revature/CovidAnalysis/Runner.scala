c6831718e95c1a2eb9bf3d2b17a0df5fe18db956
Tim Miller <timothy.miller@revature.net>
2021-05-28 10:22:13 -0400

    Ran the allGrowthFactorDataPoints function on time_series_covid_19_confirmed_US.csv

--- src/main/scala/com/revature/CovidAnalysis/Runner.scala
+++ src/main/scala/com/revature/CovidAnalysis/Runner.scala
@@ -6,7 +6,7 @@ import loadpath.LoadPath
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{array, avg, col, desc, explode, lit, struct}
+import org.apache.spark.sql.functions.{array, avg, col, desc, explode, lit, max, min, struct}
 import org.apache.spark.sql.types.{DecimalType, DoubleType, FloatType, IntegerType, StringType}
 
 import java.text.SimpleDateFormat
@@ -65,20 +65,26 @@ object Runner {
       .load(LoadPath.hdfs_path + "time_series_covid_19_recovered.csv")
       .toDF()
 
-    covid_accum_DB.show()
-    covid_confirmed_US_DB.show()
-    covid_confirmed_DB.show()
-    covid_deaths_DB.show()
-    covid_deaths_US_DB.show()
-    covid_recovered_DB.show()
+//    covid_accum_DB.show()
+//    covid_confirmed_US_DB.show()
+//    covid_confirmed_DB.show()
+//    covid_deaths_DB.show()
+//    covid_deaths_US_DB.show()
+//    covid_recovered_DB.show()
 
-    var stateName = "New Hampshire"
-    var date = "11/1/20"
-    val stateDF = covid_confirmed_US_DB.filter('Province_State===stateName)
-    changeOverTime(stateDF, date, 14).show
-    growthFactor(stateDF, date).show
-    stateDF.show
-    //println(greatestGrowthFactor(stateDF))
+    //var stateName = "New Hampshire"
+    //var date = "11/28/20"
+    //val stateDF = covid_confirmed_US_DB.filter('Province_State===stateName)
+    //changeOverTime(stateDF, date, 14).show
+    //growthFactor(stateDF, date).show
+    //stateDF.show
+    //println(greatestMedianGrowthFactor(stateDF, stateName))
+
+    val stateDF = covid_confirmed_US_DB.select(covid_confirmed_US_DB("Province_State")).distinct()
+    val stateList = stateDF.collect.map(_(0)).toList
+    println(stateList)
+    //stateList.foreach(f=>allGrowthFactorDataPoints(covid_confirmed_US_DB.filter('Province_State===f.toString).na.fill("").filter('Admin2=!="Unassigned"),"USA", f.toString,"Confirmed Cases"))
+
     spark.close()
   }
 
@@ -104,12 +110,13 @@ object Runner {
     c.add(Calendar.DATE, -1)
     val dt = format.format(c.getTime())
     var df_mod = df.select(df("Combined_Key").as("_n_"), df(strDate).cast(IntegerType)).orderBy(desc(strDate))
-    var df_temp = df.select(df("Combined_Key").as("Location"), df(dt).cast(IntegerType)).orderBy(desc(dt))
+    val df_temp = df.select(df("Combined_Key").as("Location"), df(dt).cast(IntegerType)).orderBy(desc(dt))
     df_mod = df_temp.join(df_mod, df_temp("Location")===df_mod("_n_"), "inner").drop("_n_")
     df_mod.withColumn("delta",(((df_mod(strDate)/df_mod(dt))-1)*100).cast(DecimalType(8, 2))).na.fill(0)
   }
-  //super expensive and time consuming (should probably run this, save the output, and show the output in the demo as opposed to running it during the live demo)
-  def greatestGrowthFactor(df: DataFrame) ={
+  //DO NOT RUN THESE FUNCTIONS
+  /*super expensive and time consuming
+  def greatestMedianGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
     val format = new SimpleDateFormat("M/d/yy")
     val date = format.parse("1/23/20")
     val c = Calendar.getInstance()
@@ -131,7 +138,222 @@ object Runner {
       dt = format.format(c.getTime())
     }
     tempDF = growthFactor(df,greatestDate)
-    tempDF.show
+    tempDF.coalesce(1).write.option("header", "true").csv("output/"+Country+"/"+Province+"/"+dataName+"/greatestMedGF.csv")
     greatestMedianGrowthFactor
   }
+  def greatestAvgGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var greatestDate = ""
+    var x = 0
+    var tempDF = df
+    var greatestAvgGrowthFactor = -1000000.0
+    var avgGrowthFactor = 0.0
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      avgGrowthFactor = tempDF.select(avg("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (avgGrowthFactor > greatestAvgGrowthFactor) {
+        greatestAvgGrowthFactor = avgGrowthFactor
+        greatestDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+    tempDF = growthFactor(df,greatestDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/greatestAvgGF.csv")
+    greatestAvgGrowthFactor
+  }
+  def leastMedianGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var leastDate = ""
+    var x = 0
+    var tempDF = df
+    var leastMedianGrowthFactor = 1000000.0
+    var medianGrowthFactor = 0.0
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      medianGrowthFactor = tempDF.select("delta").stat.approxQuantile("delta",Array(0.5),0.001).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (medianGrowthFactor < leastMedianGrowthFactor) {
+        leastMedianGrowthFactor = medianGrowthFactor
+        leastDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+    tempDF = growthFactor(df,leastDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/leastMedGF.csv")
+    leastMedianGrowthFactor
+  }
+  def leastAvgGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var leastDate = ""
+    var x = 0
+    var tempDF = df
+    var leastAvgGrowthFactor = 1000000.0
+    var avgGrowthFactor = 0.0
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      avgGrowthFactor = tempDF.select(avg("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (avgGrowthFactor < leastAvgGrowthFactor) {
+        leastAvgGrowthFactor = avgGrowthFactor
+        leastDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+    tempDF = growthFactor(df,leastDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/leastAvgGF.csv")
+    leastAvgGrowthFactor
+  }
+  def maxGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var leastDate = ""
+    var x = 0
+    var tempDF = df
+    var maxGrowthFactor = -1000000.0
+    var gFactor = 0.0
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      gFactor = tempDF.select(max("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (gFactor > maxGrowthFactor) {
+        maxGrowthFactor = gFactor
+        leastDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+    tempDF = growthFactor(df,leastDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/maxGF.csv")
+    maxGrowthFactor
+  }
+  def minGrowthFactor(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var leastDate = ""
+    var x = 0
+    var tempDF = df
+    var minGrowthFactor = 1000000.0
+    var gFactor = 0.0
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      gFactor = tempDF.select(max("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (gFactor < minGrowthFactor) {
+        minGrowthFactor = gFactor
+        leastDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+    tempDF = growthFactor(df,leastDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/maxGF.csv")
+    minGrowthFactor
+  }
+
+  def allGrowthFactorDataPoints(df: DataFrame, Country:String,Province:String, dataName:String) ={
+    val format = new SimpleDateFormat("M/d/yy")
+    val date = format.parse("1/23/20")
+    val c = Calendar.getInstance()
+    c.setTime(date)
+    var dt = format.format(c.getTime())
+    var greatestMedDate = ""
+    var x = 0
+    var tempDF = df
+    var greatestMedianGrowthFactor = -1000000.0
+    var medianGrowthFactor = 0.0
+
+    var greatestAvgDate = ""
+    var greatestAvgGrowthFactor = -1000000.0
+    var avgGrowthFactor = 0.0
+
+    var leastMedDate = ""
+    var leastMedianGrowthFactor = 1000000.0
+
+    var leastAvgDate = ""
+    var leastAvgGrowthFactor = 1000000.0
+
+    var maxGrowthDate = ""
+    var maxGrowthFactor = -1000000.0
+    var gFactor = 0.0
+
+    var minGrowthDate = ""
+    var minGrowthFactor = 1000000.0
+
+    for ( x <- 0 to 465){
+      tempDF = growthFactor(df,dt)
+      medianGrowthFactor = tempDF.select("delta").stat.approxQuantile("delta",Array(0.5),0.001).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (medianGrowthFactor > greatestMedianGrowthFactor) {
+        greatestMedianGrowthFactor = medianGrowthFactor
+        greatestMedDate = dt
+      }
+
+      avgGrowthFactor = tempDF.select(avg("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (avgGrowthFactor > greatestAvgGrowthFactor) {
+        greatestAvgGrowthFactor = avgGrowthFactor
+        greatestAvgDate = dt
+      }
+
+      //medianGrowthFactor = tempDF.select("delta").stat.approxQuantile("delta",Array(0.5),0.001).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (medianGrowthFactor < leastMedianGrowthFactor) {
+        leastMedianGrowthFactor = medianGrowthFactor
+        leastMedDate = dt
+      }
+
+      //avgGrowthFactor = tempDF.select(avg("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (avgGrowthFactor < leastAvgGrowthFactor) {
+        leastAvgGrowthFactor = avgGrowthFactor
+        leastAvgDate = dt
+      }
+
+      gFactor = tempDF.select(max("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (gFactor > maxGrowthFactor) {
+        maxGrowthFactor = gFactor
+        maxGrowthDate = dt
+      }
+
+      gFactor = tempDF.select(min("delta")).take(1).apply(0).toString().replaceAll("[^a-zA-Z0-9\\s\\:.-]", "").toDouble
+      if (gFactor < minGrowthFactor) {
+        minGrowthFactor = gFactor
+        minGrowthDate = dt
+      }
+      c.add(Calendar.DATE, 1)
+      dt = format.format(c.getTime())
+    }
+
+    tempDF = growthFactor(df,greatestMedDate)
+    tempDF.coalesce(1).write.option("header", "true").csv("output/"+Country+"/"+Province+"/"+dataName+"/greatestMedGF.csv")
+
+    tempDF = growthFactor(df,greatestAvgDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/greatestAvgGF.csv")
+
+    tempDF = growthFactor(df,leastMedDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/leastMedGF.csv")
+
+    tempDF = growthFactor(df,leastAvgDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/leastAvgGF.csv")
+
+    tempDF = growthFactor(df,maxGrowthDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/maxGF.csv")
+
+    tempDF = growthFactor(df,minGrowthDate)
+    tempDF.coalesce(1).write.csv("output/"+Country+"/"+Province+"/"+dataName+"/minGF.csv")
+  }
+  */
 }
